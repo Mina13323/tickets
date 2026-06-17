72f6afe1ef48816e52e20b742df2f9d0710c0c3c
Mina Wael <mena_w10052@cic-cairo.com>
2026-06-17 15:50:42 +0300

    Configure Vercel Monorepo deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'sonner';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.PROD ? '/api' : 'http://localhost:5000/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
